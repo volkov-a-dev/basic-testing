64526e2ac03f0e155f9b210e4ec52ec7a6221ca0
aliaksei.volkav <aliaksei.volkav@ventionteams.com>
2024-02-12 23:39:37 +0100

    [06]

--- src/06-mocking-node-api/index.test.ts
+++ src/06-mocking-node-api/index.test.ts
@@ -1,7 +1,12 @@
 // Uncomment the code below and write your tests
-// import { readFileAsynchronously, doStuffByTimeout, doStuffByInterval } from '.';
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { doStuffByTimeout, doStuffByInterval } from '.';
 
 describe('doStuffByTimeout', () => {
+  let callback: typeof jest.fn;
+  const timeOut = 2000;
+
   beforeAll(() => {
     jest.useFakeTimers();
   });
@@ -12,10 +17,19 @@ describe('doStuffByTimeout', () => {
 
   test('should set timeout with provided callback and timeout', () => {
     // Write your test here
+    doStuffByTimeout(callback, timeOut);
+    jest.advanceTimersByTime(timeOut);
+    expect(callback).toHaveBeenCalled();
   });
 
   test('should call callback only after timeout', () => {
     // Write your test here
+    doStuffByTimeout(callback, timeOut);
+    expect(callback).not.toHaveBeenCalled();
+    jest.advanceTimersByTime(timeOut - 1);
+    expect(callback).not.toHaveBeenCalled();
+    jest.advanceTimersByTime(1);
+    expect(callback).toHaveBeenCalled();
   });
 });
 
@@ -28,12 +42,20 @@ describe('doStuffByInterval', () => {
     jest.useRealTimers();
   });
 
+  let callback: typeof jest.fn;
+  const timeOut = 1000;
   test('should set interval with provided callback and timeout', () => {
     // Write your test here
+    doStuffByInterval(callback, timeOut);
+    jest.advanceTimersByTime(timeOut);
+    expect(callback).toHaveBeenCalled();
   });
 
   test('should call callback multiple times after multiple intervals', () => {
     // Write your test here
+    doStuffByInterval(callback, timeOut);
+    jest.advanceTimersByTime(timeOut * 2);
+    expect(callback).toHaveBeenCalledTimes(2);
   });
 });
 
